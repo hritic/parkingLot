55958bc810a9c543a50627b5783e650ee81baeb0
hritic <khritik37@gmail.com>
2022-05-02 17:05:12 +0530

    parking.h updated

--- parking.h
+++ parking.h
@@ -6,12 +6,13 @@ using namespace std;
 
 class Parking {
 private:
-    int parkingSlot;
+    int parkingSlot = 20;
     int status[parkingSlot];
+    string parkedVehicle[parkingSlot];
 
 public:
     string parkVehicle();
     void removeVehicle();
-    double generateBill();
+    //double generateBill();
     void menu();
 }
